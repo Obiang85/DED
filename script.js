3c30e55a5edaaf1c9f6d57d74592ababaf1d34a5
Obiang85 <josenguemaobiangavomo@gmail.com>
2024-01-13 18:24:57 +0000

    DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+var btnvar1=document.getElementById('btn1');
+
+  function toggle1() {
+    if (btnvar1.style.color =="red"){
+        btnvar1.style.color ="grey"
+    }
+    else {
+        btnvar1.style.color ="red"
+    }
+  }
